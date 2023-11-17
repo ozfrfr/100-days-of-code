42183e697f357003b240c8e17d01d5a31ced6fe1
hosanna57 <hosannacchigbu@gmail.com>
2023-11-17 19:57:47 +0100

    day1

--- day1.py
+++ day1.py
@@ -1,10 +1,15 @@
-#print(len(input("what is your name? ")))
+#function to print the number of chars in a string
+print(len(input("what is your name? ")))
+#or
+name = input("what is your name? ")
+len = len(name)
 
-# name = input("what is you name? ")
-# len = len(name)
-# print(f"there are {len} chars in {name}")
+print(f"there are {len} chars in {name}")
 
+#write a function to generate a band name using your city and pet name
 print("function to generate a band name")
 city = input("what city where you born? ")
 pet = input("what is your pet's name? ")
+
+#using concatenation to print the string 
 print("your band name is " + city + " " + pet )
