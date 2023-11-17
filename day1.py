3b38ab9ccddb80b78101e50d20eb43be0dcc4e21
hosanna57 <hosannacchigbu@gmail.com>
2023-11-17 07:56:49 +0100

    day 1

---
+++ day1.py
@@ -0,0 +1,10 @@
+#print(len(input("what is your name? ")))
+
+# name = input("what is you name? ")
+# len = len(name)
+# print(f"there are {len} chars in {name}")
+
+print("function to generate a band name")
+city = input("what city where you born? ")
+pet = input("what is your pet's name? ")
+print("your band name is " + city + " " + pet )
