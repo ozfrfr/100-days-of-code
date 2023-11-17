0ace3e6e52a63adce6629b49517abe88b4243169
hosanna57 <hosannacchigbu@gmail.com>
2023-11-17 20:17:39 +0100

    day2

--- day2.py
+++ day2.py
@@ -3,10 +3,13 @@
 
 num = input("Enter a two digit number ")
 
-#subscripting the first an second int in num
+#subscripting the first an second str in num
 first = (num[0])
 second = (num[1])
 
+#changing type from str to int and adding them together
 sum = int(first) + int(second)
 
 print(f"{first} + {second} = {sum}")
+
+
