d470afb4c4c8a9eb994a8a02587ec49e0c3b78f1
hosanna57 <hosannacchigbu@gmail.com>
2023-11-18 15:21:29 +0100

    day3

---
+++ day3.py
@@ -0,0 +1,9 @@
+# function to know if a number is even
+
+print("Check if a number ris even or not")
+num = int(input("Enter a number "))
+
+if num % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is not even")
