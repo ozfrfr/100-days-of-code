1f08f5563a040f25064e1d11246df4f9453f9afe
hosanna57 <hosannacchigbu@gmail.com>
2023-11-25 08:52:39 +0100

    update

--- day9-Secret_Auction.py
+++ day9-Secret_Auction.py
@@ -26,31 +26,26 @@ name = input("What is your name? ")
 bid = input("What is your bid price? $")
 os.system('clear')
 
-auction = [
-{
-    "Name": name,
-    "Bid": bid
-}
-]
-
-ask = input("Are there other users who want to bid? (y/n) ").lower()
+ask = input("Are there other users who want to bid? Type 'Yes' or 'No'. ").lower()
 os.system('clear')
 
-while ask not in ['y', 'n']:
+while ask not in ['yes', 'no']:
     os.system('clear')
-    print("Invalid input. Please enter 'y' or 'n'.")
-    ask = input("Are there other users who want to bid? (y/n) ").lower()
+    print("Invalid input. Please enter 'Yes' or 'No'.")
+    ask = input("Are there other users who want to bid? Type 'Yes' or 'No'. ").lower()
 
-while ask == "y":
+auction = []
+
+while ask == "yes":
     os.system('clear')
-    new_entry = {}
-    new_name = input("What is your name? ")
-    new_entry["Name"] = new_name
-    new_bid = input("What is your bid price? $")
-    new_entry["Bid"] = new_bid
-    auction.append(new_entry)
+    entry = {}
+    name = input("What is your name? ")
+    entry["Name"] = name
+    bid = input("What is your bid price? $")
+    entry["Bid"] = bid
+    auction.append(entry)
     os.system('clear')
-    ask = input("Are there other users who want to bid? (y/n) ").lower()
+    ask = input("Are there other users who want to bid? 'Yes' or 'No'. ").lower()
 else:
     os.system('clear') 
     from operator import itemgetter
