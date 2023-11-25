ce775f7dfef5f4c7c5b869fda8bbe79447af6ba0
hosanna57 <hosannacchigbu@gmail.com>
2023-11-25 10:35:06 +0100

    auction_update!

--- day9-Secret_Auction.py
+++ day9-Secret_Auction.py
@@ -42,16 +42,16 @@ while ask not in ['yes', 'no']:
     print("Invalid input. Please enter 'Yes' or 'No'.")
     ask = input("Are there other users who want to bid? Type 'Yes' or 'No'. ").lower()
 
-while ask == "yes":
-    os.system('clear')
-    new_entry = {}
-    new_name = input("What is your name? ")
-    new_entry["Name"] = new_name
-    new_bid = input("What is your bid price? $")
-    new_entry["Bid"] = int(new_bid)
-    auction.append(new_entry)
-    os.system('clear')
-    ask = input("Are there other users who want to bid? 'Yes' or 'No'. ").lower()
+    while ask == "yes":
+        os.system('clear')
+        new_entry = {}
+        new_name = input("What is your name? ")
+        new_entry["Name"] = new_name
+        new_bid = input("What is your bid price? $")
+        new_entry["Bid"] = int(new_bid)
+        auction.append(new_entry)
+        os.system('clear')
+        ask = input("Are there other users who want to bid? 'Yes' or 'No'. ").lower()
 
 if ask == "no":
     os.system('clear') 
@@ -60,6 +60,5 @@ if ask == "no":
 
     winner_name = winner['Name']
     winner_bid = winner['Bid']
-
 
     print(f"The winner is {winner_name} with bid of ${winner_bid}\n")
