91360d7ae2efa8fdf4ae1ac7c401ecbcfb75192a
hosanna57 <hosannacchigbu@gmail.com>
2023-11-29 11:50:24 +0100

    update

---
+++ Day14-Higher_Lower/main.py
@@ -0,0 +1,57 @@
+import os
+import random
+from art import logo
+from art import vs
+from game_data import data
+
+def compare_a_message():
+    name_a = a['name']
+    description_a = a['description']
+    country_a = a['country']
+    return f"Compare A: {name_a}, a {description_a}, from {country_a}"
+
+def compare_b_message():
+    name_b = b['name']
+    description_b = b['description']
+    country_b = b['country']
+    return f"Compare B: {name_b}, a {description_b}, from {country_b}"
+
+def highest():
+    max_followers = max([followers_a_count, followers_b_count])
+    return max_followers
+
+score = 0
+game_end = False
+
+while not game_end:
+    a = random.choice(data)
+    followers_a_count = a['follower_count']
+    b = random.choice(data)
+    followers_b_count = b['follower_count']
+
+    print(logo)
+    score = 0
+    if score > 0:
+        print(f"You're right! Current score: {score}")
+
+    print(compare_a_message())
+    print(vs)
+    print(compare_b_message())
+
+    user_input = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+    if user_input == "a":
+        user_choice = int(followers_a_count)
+    elif user_input == "b":
+        user_choice = int(followers_b_count)
+
+    higher = int(highest())
+
+    if user_choice == higher:
+        score += 1
+        game_end = False
+    else:
+        os.system('clear')
+        print(logo)
+        print(f"Sorry thats wrong. Final score: {score}")
+        game_end = True
