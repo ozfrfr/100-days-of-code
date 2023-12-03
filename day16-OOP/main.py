dec7fa1701728ae75d710c5e3c6bd1be13eb13df
hosanna57 <hosannacchigbu@gmail.com>
2023-12-03 08:40:00 +0100

    update

--- day16-OOP/main.py
+++ day16-OOP/main.py
@@ -1,24 +1,12 @@
-# import turtle
-#
-# timmy = turtle.Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("coral")
-# timmy.forward(100)
-#
-# my_screen = turtle.Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+import turtle
 
-from prettytable import PrettyTable
+timmy = turtle.Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
 
-table = PrettyTable()
-
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander",])
-table.add_column("Type", ["Electric", "Water", "Fire"])
-
-table.align = "l"
-
-print(table)
-
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
 
